88319ed65adaf2451489ed3f487e445e117487b0
RajkumarSubramani <rajs.4391@gmail.com>
2017-03-31 00:58:53 +0530

    Style Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
 class App extends Component {
+    constructor(props){
+        super(props);
+      this.state={  query: '' }
+    }
+    search(){
+        console.log('this state', this.state);
+    }
     render(){
         return(
               <div>
-                  App
+               <div className="App container">
+                   <h3>Music Artist</h3>
+                   <FormGroup>
+                       <InputGroup>
+                       <FormControl type="text"  placeholder="Search" value={this.state.query}
+                       onChange={event =>{this.setState({query:event.target.value})}} 
+                       onKeyPress={event =>{
+                           if(event.key === 'Enter'){
+                             this.search() 
+                           }
+                       }} /> 
+                       <InputGroup.Addon onClick={() => this.search()}>
+                       <Glyphicon glyph="search"></Glyphicon>
+                       </InputGroup.Addon>
+                       </InputGroup>
+                   </FormGroup>
+                   <div className="profile">
+                       <div>Pic</div>
+                       <div>A.Name</div>
+                       </div>
+                       <div className="Gallery">
+                           Gallery
+                           </div>
+                   </div>
                   </div>  
         )
     }
